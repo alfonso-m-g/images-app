e266442b1d0682485a2121721d2b89e5dafd723d
alfonso-m-g <alfonso96xd@gmail.com>
2022-01-22 13:34:14 -0600

    Modified main.js

--- main.js
+++ main.js
@@ -22,26 +22,59 @@ let frases = [
               "alfonso",
               "alfonso"
             ];
+
 for (i=1; i<=21; i++){
     string = string + "<div class='item' id='" + i + "'><img src='/images/" + i + ".JPG' alt='te amo'><h3>" + frases[i-1] + "</h3></div>";
 }
+
 document.getElementById("place-pic").innerHTML = string;
-
-
-
 
 function accion()
 {
+    
+    let value;
+    let exec_count;
+    let test = localStorage.getItem('pressed');
+    let exec_count_storage = localStorage.getItem('count');
+
+    if(test == null){
+        value = 'false';
+        exec_count = 0;
+    }
+    else{
+        value = test;
+        exec_count = exec_count_storage;
+    }
+
+    console.log(value);
+    console.log(exec_count);
     scroll(0,0);
     let num=1;
     const timeValue = setInterval(function(){ 
 
-            document.getElementById(num).innerHTML = "<img src='/images/wola.png'></img>";
+            if(value == 'false'){
+                document.getElementById(num).innerHTML = "<img src='/images/wola.png'></img>";
+            }
+
+            if(value == 'true'){
+
+                if(exec_count % 2 == 1){
+                    document.getElementById(num).innerHTML = "<img src='/images/" + num + ".jpg'></img>";
+                }
+
+                if (exec_count % 2 == 0){
+                    document.getElementById(num).innerHTML = "<img src='/images/wola.png'></img>";
+                }
+            }
 
             if(num == 21){
                 clearInterval(timeValue);
+                exec_count++;
+                localStorage.setItem('count',exec_count);
+                localStorage.setItem('pressed',true);
+                value = localStorage.getItem('pressed');
             }
 
             num++
-    }, 500);  
+    }, 750);  
 }
