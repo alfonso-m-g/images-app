31a1756dfb247e98ed4b6cc0219b91c681d2d3a1
alfonso-m-g <alfonso96xd@gmail.com>
2022-01-22 13:35:57 -0600

    Modified main.js

--- main.js
+++ main.js
@@ -31,7 +31,6 @@ document.getElementById("place-pic").innerHTML = string;
 
 function accion()
 {
-    
     let value;
     let exec_count;
     let test = localStorage.getItem('pressed');
@@ -50,14 +49,13 @@ function accion()
     console.log(exec_count);
     scroll(0,0);
     let num=1;
-    const timeValue = setInterval(function(){ 
 
+    const timeValue = setInterval(function(){ 
             if(value == 'false'){
                 document.getElementById(num).innerHTML = "<img src='/images/wola.png'></img>";
             }
 
             if(value == 'true'){
-
                 if(exec_count % 2 == 1){
                     document.getElementById(num).innerHTML = "<img src='/images/" + num + ".jpg'></img>";
                 }
@@ -74,7 +72,6 @@ function accion()
                 localStorage.setItem('pressed',true);
                 value = localStorage.getItem('pressed');
             }
-
             num++
     }, 750);  
 }
